527044c98434d396f4baf7601ec501822295be5f
pedrogommes <40220180@ESMAD.IPP.PT>
2024-01-23 01:19:14 +0000

    fix: fixed videoPlayer and episode backbutton + added styles to both

--- frontend/lib/pages/episodePage.dart
+++ frontend/lib/pages/episodePage.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import './videoPlayerPage.dart';
+import './mediaDetailsPage.dart';
 
 class EpisodePage extends StatefulWidget {
   final String mediaType;
   final String title;
+  final String mediaTitle;
   final List<Map<String, dynamic>> episodes;
 
-  EpisodePage(this.mediaType, this.title, this.episodes);
+  EpisodePage(this.mediaType, this.title, this.episodes, this.mediaTitle);
 
   @override
   _EpisodePageState createState() => _EpisodePageState();
@@ -16,27 +18,77 @@ class _EpisodePageState extends State<EpisodePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemCount: widget.episodes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Image.network(widget.episodes[index]['imageUrl']),
-            title: Text(widget.episodes[index]['title']),
-            subtitle: Text('Episode ${index + 1}'),
-            onTap: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      VideoPlayerPage(widget.episodes[index]['epUrl']),
+      backgroundColor: Color(0xFF1C1C1C),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(64.0),
+                child: Text(
+                  widget.title,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
-              );
-            },
-          );
-        },
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.episodes.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ListTile(
+                        leading: Image.network(
+                          widget.episodes[index]['imageUrl'],
+                          width: 64.0,
+                          height: 64.0,
+                        ),
+                        title: Text(
+                          widget.episodes[index]['title'],
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                        subtitle: Text(
+                          'Episode ${index + 1}',
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              color: const Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => VideoPlayerPage(
+                                  widget.episodes[index]['epUrl'],
+                                  widget.mediaType,
+                                  widget.title,
+                                  widget.episodes),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 54.0,
+            left: 20.0,
+            child: IconButton(
+              onPressed: () {
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                }
+              },
+              icon: Icon(Icons.arrow_back, color: Color(0xFFFA3D3B)),
+            ),
+          ),
+        ],
       ),
     );
   }
--- frontend/lib/pages/videoPlayerPage.dart
+++ frontend/lib/pages/videoPlayerPage.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:chewie/chewie.dart';
 import 'package:video_player/video_player.dart';
+import './episodePage.dart';
+import './mediaDetailsPage.dart';
 
 class VideoPlayerPage extends StatefulWidget {
   final String videoUrl;
+  final String mediaType;
+  final String mediaTitle;
+  final List<Map<String, dynamic>> episodes;
 
-  VideoPlayerPage(this.videoUrl);
+  VideoPlayerPage(this.videoUrl, this.mediaType, this.mediaTitle, this.episodes,
+      {Key? key})
+      : super(key: key);
 
   @override
   _VideoPlayerPageState createState() => _VideoPlayerPageState();
@@ -43,10 +50,40 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Chewie(
-          controller: _chewieController,
-        ),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Chewie(
+              controller: _chewieController,
+            ),
+          ),
+          Positioned(
+            right: 330,
+            top: 50,
+            child: FloatingActionButton(
+              onPressed: () {
+                if (widget.mediaType == 'animes') {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EpisodePage(widget.mediaType,
+                            widget.mediaTitle, widget.episodes, widget.videoUrl)),
+                  );
+                } else if (widget.mediaType == 'films') {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            MediaDetailsPage(widget.mediaType, widget.mediaTitle)),
+                  );
+                }
+              },
+              backgroundColor: Color(0xFF1C1C1C),
+              child: Icon(Icons.arrow_back, color: Color(0xFFFA3D3B)),
+            ),
+          ),
+        ],
       ),
     );
   }
