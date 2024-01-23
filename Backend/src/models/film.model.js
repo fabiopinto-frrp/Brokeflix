b5839a1237593a0db0e8d91b26ce81086e762901
unknown <40220296@esmad.ipp.pt>
2024-01-23 01:20:05 +0000

    fix: filmModel in videoUrl to default: empty

--- Backend/src/models/film.model.js
+++ Backend/src/models/film.model.js
@@ -27,7 +27,7 @@ const filmSchema = new Schema({
   status: { type: String, required: true },
   imageUrl: { type: String, required: true },
   backgroundImgUrl: { type: String, required: true },
-  videosUrl: { type: String, default: "", required: false },
+  videosUrl: { type: String, default: "Empty", required: false },
 });
 
 const FilmModel = db.model("films", filmSchema);
