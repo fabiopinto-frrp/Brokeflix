e34456533a3fae6bf9a8a22a3e6fb77c45dfd175
unknown <40220296@esmad.ipp.pt>
2024-01-23 00:47:34 +0000

    fix: fixed SearchPage

--- frontend/lib/pages/searchPage.dart
+++ frontend/lib/pages/searchPage.dart
@@ -17,47 +17,62 @@ class SearchPage extends StatefulWidget {
 
 class SearchPageState extends State<SearchPage> {
   String query = '';
-  late Future<List<dynamic>> futureSearchResults;
   final storage = const FlutterSecureStorage();
+
+  List<dynamic> allResults = [];
+  List<dynamic> filteredResults = [];
 
   @override
   void initState() {
     super.initState();
-    futureSearchResults = fetchSearchResults(query);
+    fetchSearchResults('');
   }
 
-  Future<List<dynamic>> fetchSearchResults(String query) async {
+  Future<void> fetchSearchResults(String query) async {
     List<String> mediaTypes = ['animes', 'films', 'series'];
-    List<dynamic> allResults = [];
+    allResults.clear();
 
     for (String mediaType in mediaTypes) {
       var url = Uri.parse(
           'https://brokeflix-api.tech/api/$mediaType?query=${Uri.encodeComponent(query)}');
-      print('Fetching results from $url'); // Debug print
+      print('Fetching results from $url');
       try {
         var response = await http.get(url);
         if (response.statusCode == 200) {
           var results = jsonDecode(response.body);
-          print('Got ${results.length} results'); // Debug print
+          print('Got ${results.length} results');
           allResults.addAll(
               results.map((result) => {...result, 'mediaType': mediaType}));
         } else {
           throw Exception('Failed to load search results');
         }
       } catch (e) {
-        print('Error fetching results: $e'); // Debug print
+        print('Error fetching results: $e');
       }
     }
 
-    return allResults;
+    setState(() {
+      filteredResults = List.from(allResults);
+    });
   }
 
   void updateQuery(String newQuery) {
-    print('Updating query to $newQuery'); // Debug print
-    setState(() {
-      query = newQuery;
-      futureSearchResults = fetchSearchResults(query);
-    });
+    print('Updating query to $newQuery');
+
+    if (newQuery.isEmpty) {
+      setState(() {
+        query = '';
+        filteredResults = List.from(allResults);
+      });
+    } else {
+      setState(() {
+        query = newQuery;
+        filteredResults = allResults
+            .where((result) =>
+                result['title'].toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      });
+    }
   }
 
   @override
@@ -67,47 +82,48 @@ class SearchPageState extends State<SearchPage> {
       builder: (context, child) => Scaffold(
         appBar: TopBar(),
         backgroundColor: const Color(0xFF1C1C1C),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Your search input field
-              TextField(
-                onChanged: updateQuery,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: 'Search...',
+        body: ListView(
+          children: [
+            Theme(
+              data: Theme.of(context).copyWith(
+                inputDecorationTheme: InputDecorationTheme(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFFA3D3B)),
+                  ),
                 ),
               ),
-              // Display mixed search results
-              SizedBox(
-                height: 200.h,
-                child: FutureBuilder<List<dynamic>>(
-                  future: futureSearchResults,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: MediaCard(
-                              mediaType: snapshot.data![index]["mediaType"],
-                              mediaImgUrl: snapshot.data![index]["imageUrl"],
-                              mediaTitle: snapshot.data![index]["title"],
-                            ),
-                          );
-                        },
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-                    return CircularProgressIndicator();
-                  },
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextField(
+                  onChanged: updateQuery,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 20.0),
+                    hintText: 'Search...',
+                    hintStyle: TextStyle(color: Color(0xFFFA3D3B)),
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 30,
+              ),
+              itemCount: filteredResults.length,
+              itemBuilder: (context, index) {
+                return MediaCard(
+                  mediaType: filteredResults[index]["mediaType"],
+                  mediaImgUrl: filteredResults[index]["imageUrl"],
+                  mediaTitle: filteredResults[index]["title"],
+                );
+              },
+            ),
+          ],
         ),
         bottomNavigationBar: const BottomBar(),
       ),
